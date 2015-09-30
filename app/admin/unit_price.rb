30dcb805e668f7e36476b82aa6c34d6dcf55c7ea
Prelang Builder <builder@prelang.com>
2015-09-30 21:22:43 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'UnitPrice' to allow for create, update

--- app/admin/unit_price.rb
+++ app/admin/unit_price.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register UnitPrice do
+  permit_params :abv, :volume, :units, :price, :unitprice
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
